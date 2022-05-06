b96b953da9aec671db0c5ddbe29f8905d7c48848
hmalmsten <98034456+hmalmsten@users.noreply.github.com>
2022-05-06 17:23:17 +0300

    Delete part2/phonebook-full directory

--- part2/phonebook-full/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({ showSome, handleFilterChange }) => {
-    return (
-        <form>
-        filter <input value={showSome} onChange={handleFilterChange}/>
-        </form>
-    )
-  }
-  
-  export default Filter
