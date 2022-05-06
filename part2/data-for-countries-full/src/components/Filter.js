0e4d622288c981095bf75765b3ef55e36f9ecc66
hmalmsten <98034456+hmalmsten@users.noreply.github.com>
2022-05-06 17:20:33 +0300

    Delete part2/data-for-countries-full directory

--- part2/data-for-countries-full/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({ showSome, handleFilterChange }) => {
-    return (
-        <form>
-        find countries <input value={showSome} onChange={handleFilterChange}/>
-        </form>
-    )
-  }
-  
-  export default Filter
